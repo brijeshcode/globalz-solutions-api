bd17468c6f55a5097cfd6794316c99afd373ea0a
brijeshcode <brijeshchaturvedi.it@gmail.com>
2025-10-04 20:19:19 +0530

    new view update

---
+++ public/assets/Create--5LT1wmQ.js
@@ -0,0 +1,1 @@
+import"./GlobalConfig-Dep5CV3r.js";import"./tableUtils-jdfVzp2e.js";import"./itemTypesStore-DrfUDZWA.js";import{b as e}from"./Create-DCkE3DC6.js";export{e as default};
--- public/assets/Create-srwkIams.js
+++
@@ -1,1 +0,0 @@
-import"./GlobalConfig-Dep5CV3r.js";import"./tableUtils-jdfVzp2e.js";import"./itemTypesStore-duOPJIXi.js";import{b as e}from"./Create-CJKqOHq8.js";export{e as default};
