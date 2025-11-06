c1c945ab849b1999bcd2eb088be0942786e8c84b
brijeshcode <brijeshchaturvedi.it@gmail.com>
2025-11-07 04:54:15 +0530

    scheduler test added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,6 +14,17 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+    })
+    ->withSchedule(function (Schedule $schedule): void {
+        // Calculate monthly closing balance at 00:01 on the 1st of each month
+        // $schedule->command('customers:calculate-monthly-closing')
+        //     ->monthlyOn(1, '00:01')
+        //     ->withoutOverlapping()
+        //     ->runInBackground();
+
+        $schedule->call(function () {
+            info('Scheduler is working!');
+            })->everyMinute();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
