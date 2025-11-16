0f81e0f4d3a14c7968d3d72844aa4de87b2753f0
brijeshcode <brijeshchaturvedi.it@gmail.com>
2025-11-16 08:23:29 +0530

    account balance update event added

--- app/Models/Accounts/AccountTransfer.php
+++ app/Models/Accounts/AccountTransfer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Accounts;
 
+use App\Helpers\AccountsHelper;
 use App\Models\Setups\Generals\Currencies\Currency;
 use App\Models\User;
 use App\Traits\Authorable;
@@ -106,6 +107,40 @@ class AccountTransfer extends Model
                 $transfer->setTransferCode();
             }
             $transfer->prefix = 'TRF';
+        });
+
+        static::created(function ($transfer) { 
+            AccountsHelper::removeBalance(Account::find($transfer->from_account_id), $transfer->sent_amount);
+ 
+            AccountsHelper::addBalance(Account::find($transfer->to_account_id), $transfer->received_amount);
+        });
+
+        static::updated(function ($transfer) {
+            $original = $transfer->getOriginal();
+
+            if ($original['from_account_id'] != $transfer->from_account_id) {
+                AccountsHelper::addBalance(Account::find($original['from_account_id']), $original['sent_amount']);
+                AccountsHelper::removeBalance(Account::find($transfer->from_account_id), $transfer->sent_amount);
+            }
+            elseif ($original['sent_amount'] != $transfer->sent_amount) {
+                $difference = $transfer->sent_amount - $original['sent_amount'];
+                AccountsHelper::removeBalance(Account::find($transfer->from_account_id), $difference);
+            }
+
+            if ($original['to_account_id'] != $transfer->to_account_id) {
+                AccountsHelper::removeBalance(Account::find($original['to_account_id']), $original['received_amount']);
+                AccountsHelper::addBalance(Account::find($transfer->to_account_id), $transfer->received_amount);
+            }
+            elseif ($original['received_amount'] != $transfer->received_amount) {
+                $difference = $transfer->received_amount - $original['received_amount'];
+                AccountsHelper::addBalance(Account::find($transfer->to_account_id), $difference);
+            }
+        });
+
+        static::deleted(function ($transfer) { 
+            AccountsHelper::addBalance(Account::find($transfer->from_account_id), $transfer->sent_amount);
+ 
+            AccountsHelper::removeBalance(Account::find($transfer->to_account_id), $transfer->received_amount);
         });
     }
 }
