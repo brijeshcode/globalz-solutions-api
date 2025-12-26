73dfac22ec7e8570f90b5fc4a4f8cd6bbe5701af
brijeshcode <brijeshchaturvedi.it@gmail.com>
2025-12-26 22:47:14 +0530

    Admin role: employee edit allowed

--- app/Http/Requests/Api/Employees/EmployeesUpdateRequest.php
+++ app/Http/Requests/Api/Employees/EmployeesUpdateRequest.php
@@ -12,7 +12,7 @@ class EmployeesUpdateRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return RoleHelper::canSuperAdmin();
+        return RoleHelper::canAdmin();
     }
 
     /**
