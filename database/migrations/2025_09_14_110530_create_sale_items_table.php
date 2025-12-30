683c7314a29462c07ffc5450518aff7809878ca2
brijeshcode <brijeshchaturvedi.it@gmail.com>
2025-12-30 13:46:28 +0530

    new fied added to sale

--- database/migrations/2025_09_14_110530_create_sale_items_table.php
+++ database/migrations/2025_09_14_110530_create_sale_items_table.php
@@ -18,38 +18,51 @@ return new class extends Migration
             $table->unsignedBigInteger('sale_id')->index(); // Foreign key to sales table
             $table->unsignedBigInteger('item_id')->nullable()->index();
 
-            $table->quantity('quantity')->default(0);
             $table->money('cost_price')->default(0)->comment('in usd');
-            
-            $table->decimal('unit_volume_cbm', 10,4)->default(0);
-            $table->decimal('unit_weight_kg', 10,4)->default(0);
-            
-            // profit always in usd 
-            $table->money('unit_profit')->default(0)->comment('ttc_price_usd - cost_price (cost price is already in usd)');
 
             $table->money('price')->default(0)->comment('sale price is in selected currency');
             $table->money('price_usd')->default(0)->comment('sale price_usd use for profit calculation');
-
-            $table->money('tax_percent')->default(0)->comment('tax percent from the items table');
-            $table->string('tax_label')->default('TVA')->comment('can be TVA and No');
-            $table->money('tax_amount')->default(0)->comment('tax amount per unit ');
-            $table->money('tax_amount_usd')->default(0)->comment('tax amount usd per unit ');
-
-            $table->money('ttc_price')->default(0)->comment('ttc price will be in selected currency');
-            $table->money('ttc_price_usd')->default(0)->comment('ttc price usd');
             
             $table->percent('discount_percent')->default(0);
-            
+
             $table->money('unit_discount_amount')->default(0)->comment('in selected_currency');
             $table->money('unit_discount_amount_usd')->default(0)->comment('in usd');
             
+            $table->money('net_sell_price')->default(0)->comment('net sell price = price - unit_discount_amount');
+            $table->money('net_sell_price_usd')->default(0)->comment('net sell price usd = price_usd - unit_discount_amount_usd');
+            
+            $table->quantity('quantity')->default(0);
+
+            $table->money('tax_percent')->default(0)->comment('tax percent from the items table');
+            $table->string('tax_label')->default('TVA')->comment('can be TVA and No');
+            
+            $table->money('tax_amount')->default(0)->comment('tax amount per unit ');
+            $table->money('tax_amount_usd')->default(0)->comment('tax amount usd per unit ');
+            
+            $table->money('total_tax_amount')->default(0)->comment('tax_amount * quantity ');
+            $table->money('total_tax_amount_usd')->default(0)->comment('total_tax_amount in usd ');
+            
+            $table->money('ttc_price')->default(0)->comment('= net_sell_price + tax_amount');
+            $table->money('ttc_price_usd')->default(0)->comment('ttc price in usd');
+
+            // profit always in usd 
+            $table->money('unit_profit')->default(0)->comment('ttc_price_usd - cost_price (cost price is already in usd)');
+
+
+            $table->money('total_net_sell_price')->default(0)->comment(' = net_sell_price * quantity');
+            $table->money('total_net_sell_price_usd')->default(0)->comment(' = total_net_sell_price in usd');
+
+            $table->money('total_price')->default(0)->comment(' = ttc_price * quantity') ;// price - discount * quantity
+            $table->money('total_price_usd')->default(0)->comment(' = ttc_price_usd * quantity');
+
             $table->money('discount_amount')->default(0)->comment('in usd');
             $table->money('discount_amount_usd')->default(0)->comment('in selected_currency');
             
-            $table->money('total_price')->default(0); // price - discount * quantity
-            $table->money('total_price_usd')->default(0);
             // profit always in usd 
             $table->money('total_profit')->default(0)->comment('total_price_usd - (cost_price * quantity)');
+
+            $table->decimal('unit_volume_cbm', 10,4)->default(0);
+            $table->decimal('unit_weight_kg', 10,4)->default(0);
 
             $table->decimal('total_volume_cbm', 10,4)->default(0);
             $table->decimal('total_weight_kg', 10,4)->default(0);
