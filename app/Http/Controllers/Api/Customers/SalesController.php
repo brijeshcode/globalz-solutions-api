468170b367e3b64d515dc31c6d9e33d82ef1ee60
brijeshcode <brijeshchaturvedi.it@gmail.com>
2026-04-12 07:28:22 +0530

    updated qr code of invoice

--- app/Http/Controllers/Api/Customers/SalesController.php
+++ app/Http/Controllers/Api/Customers/SalesController.php
@@ -222,7 +222,7 @@ class SalesController extends Controller
             return ApiResponse::customError('Sale is not approved', 404);
         }
 
-        $sale->load(['saleItems.item', 'saleItems.item.itemUnit:id,name', 'saleItems.item.taxCode:id,name,code,description,tax_percent', 'warehouse:id,name', 'currency', 'priceList:id,code,description', 'customer:id,name,code,address,city,mobile,mof_tax_number', 'salesperson:id,name', 'createdBy:id,name', 'updatedBy:id,name', 'approvedBy:id,name']);
+        $sale->load(['saleItems.item', 'saleItems.item.itemUnit:id,name', 'saleItems.item.taxCode:id,name,code,description,tax_percent', 'warehouse:id,name', 'currency', 'priceList:id,code,description', 'customer:id,name,code,address,city,mobile,mof_tax_number,google_map', 'salesperson:id,name', 'createdBy:id,name', 'updatedBy:id,name', 'approvedBy:id,name']);
 
         return ApiResponse::show(
             'Sale retrieved successfully',
