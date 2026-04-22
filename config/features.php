7e1b93fc10317f3592d7c074ac1b36654df91685
brijeshcode <brijeshchaturvedi.it@gmail.com>
2026-04-22 15:24:53 +0530

    new features updated

--- config/features.php
+++ config/features.php
@@ -13,50 +13,54 @@ return [
     */
 
     // Sales & Customers
-    'sale_orders'                => true,
-    'customer_payment_orders'    => true,
-    'customer_returns'           => true,
-    'customer_return_orders'     => true,
-    'customer_credit_notes'      => true,
+    'sale_orders'                       => true,
+    'customer_payment_orders'           => true,
+    'customer_returns'                  => true,
+    'customer_return_orders'            => true,
+    'customer_credit_notes'             => true,
 
     // Purchases & Suppliers
-    'purchase_returns'           => true,
-    'supplier_credit_notes'      => true,
+    'purchase_returns'                  => true,
+    'supplier_credit_notes'             => true,
 
     // Expenses
-    'expense'                    => true,
-    'expense_deferred_payment'   => true,
+    'expense'                           => true,
+    'expense_deferred_payment'          => false,
 
     // Finance & Accounts
-    'multi_currency'             => true,
-    'income_transactions'        => true,
-    'account_transfers'          => true,
-    'account_adjustments'        => true,
+    'multi_currency'                    => true,
+    'income_transactions'               => true,
+    'account_transfers'                 => true,
+    'account_adjustments'               => true,
 
     // Inventory
-    'item_transfers'             => true,
-    'item_adjustments'           => true,
-    'price_lists'                => true,
-    'item_cost_history'          => true,
+    'item_transfers'                    => true,
+    'item_adjustments'                  => true,
+    'price_lists'                       => true,
+    'item_cost_history'                 => true,
 
     // HR / Employees
-    'employee_management'        => true,
-    'salary_management'          => true,
-    'employee_commissions'       => true,
-    'advance_loans'              => true,
+    'employee_management'               => true,
+    'salary_management'                 => true,
+    'employee_commissions'              => true,
+    'advance_loans'                     => true,
 
     // Reports
-    'report_capital'             => true,
-    'report_profit'              => true,
-    'report_employee_sales'      => true,
-    'report_expense_analysis'    => true,
-    'report_warehouse'           => true,
-    'report_sales_category'      => true,
-    'report_customer_aging'      => true,
-    'report_item_sales'          => true,
+    'report_capital'                    => true,
+    'report_profit'                     => true,
+    'report_employee_monthly_sales'     => true,
+    'report_employee_yearly_sales'      => true,
+    'report_expense_analysis'           => true,
+    'report_warehouse'                  => true,
+    'report_sales_category'             => true,
+    'report_customer_aging'             => true,
+    'report_item_sales'                 => true,
+    'vat_report'                        => true,
 
     // System
-    'activity_logs'              => true,
-    'tax_codes'                  => true,
+    'activity_logs'                     => true,
+    'tax_codes'                         => true,
+    'export_customers'                  => true,
+    'database_mirror'                   => true,
 
 ];
