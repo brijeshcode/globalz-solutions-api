13c79b0db0826070caf1e354c1c76a330edf30da
brijeshcode <brijeshchaturvedi.it@gmail.com>
2026-05-30 10:44:12 +0530

    update the template

--- resources/views/pdfs/partials/items-table-template-2.blade.php
+++ resources/views/pdfs/partials/items-table-template-2.blade.php
@@ -8,7 +8,7 @@
             <th style="width: 7%;">Qty.</th>
             <th style="width: 9%;">Price</th>
             <th style="width: 7%;">Disc.%</th>
-            <th style="width: 11%;">Net Unit Price</th>
+            {{-- <th style="width: 11%;">Net Unit Price</th> --}}
             <th style="width: 22%;">Total</th>
         </tr>
         @else
@@ -19,7 +19,7 @@
             <th style="width: 7%;">Quantity</th>
             <th style="width: 9%;">Price</th>
             <th style="width: 7%;">Disc</th>
-            <th style="width: 11%;">Net Unit Price</th>
+            {{-- <th style="width: 11%;">Net Unit Price</th> --}}
             <th style="width: 22%;">Total</th>
         </tr>
         @endif
@@ -33,7 +33,7 @@
             <td class="text-center">{{ rtrim(rtrim(number_format($item->quantity, 2), '0'), '.') }}</td>
             <td class="text-center">{{ number_format($item->price, 2) }}</td>
             <td class="text-center">{{ number_format($item->discount_percent, 2) }}%</td>
-            <td class="text-center">{{ number_format($item->net_sell_price, 2) }}</td>
+            {{-- <td class="text-center">{{ number_format($item->net_sell_price, 2) }}</td> --}}
             <td class="text-right font-bold">{{ number_format($item->total_net_sell_price, 2) }}</td>
         </tr>
         @endforeach
@@ -52,7 +52,7 @@
             <td>&nbsp;</td>
             <td>&nbsp;</td>
             <td>&nbsp;</td>
-            <td>&nbsp;</td>
+            {{-- <td>&nbsp;</td> --}}
             <td>&nbsp;</td>
         </tr>
         @endfor
