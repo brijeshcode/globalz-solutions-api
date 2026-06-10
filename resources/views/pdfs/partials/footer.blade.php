7f25c7f16c1c8872cfb81f087f07a392b67fde17
brijeshcode <brijeshchaturvedi.it@gmail.com>
2026-06-10 13:22:18 +0530

    move catalog qr to signature section

--- resources/views/pdfs/partials/footer.blade.php
+++ resources/views/pdfs/partials/footer.blade.php
@@ -6,14 +6,3 @@
         {{ $sale->invoice_nb2 }}
     @endif
 </div>
-
-@if(!empty($catalogQrCodeBase64))
-<div style="text-align: right; margin-top: 15px;">
-    @if(!empty($catalogLabel))
-    <div style="font-size: 8pt; margin-bottom: 3px;">{{ $catalogLabel }}</div>
-    @endif
-    <img src="data:image/png;base64,{{ $catalogQrCodeBase64 }}"
-         style="width: 80px; height: 80px;"
-         alt="Item Catalog">
-</div>
-@endif
--- resources/views/pdfs/partials/signature.blade.php
+++ resources/views/pdfs/partials/signature.blade.php
@@ -1,5 +1,27 @@
 @if($sale->prefix !== 'INX')
-<div class="signature-section clearfix">
+@if(!empty($catalogQrCodeBase64))
+<div class="signature-section clearfix" style="display: flex; align-items: flex-end; justify-content: space-between;">
+    <div style="display: flex;">
+        <div class="signature-box">
+            <div><strong>{{ __('invoice.received_by') }}</strong></div>
+            <div class="signature-line"></div>
+        </div>
+        <div class="signature-box">
+            <div><strong>{{ __('invoice.signature') }}</strong></div>
+            <div class="signature-line"></div>
+        </div>
+    </div>
+    <div style="text-align: right;">
+        @if(!empty($catalogLabel))
+        <div style="font-size: 8pt; margin-bottom: 3px;">{{ $catalogLabel }}</div>
+        @endif
+        <img src="data:image/png;base64,{{ $catalogQrCodeBase64 }}"
+             style="width: 80px; height: 80px;"
+             alt="Item Catalog">
+    </div>
+</div>
+@else
+<div class="signature-section clearfix" style="display: flex;">
     <div class="signature-box">
         <div><strong>{{ __('invoice.received_by') }}</strong></div>
         <div class="signature-line"></div>
@@ -9,4 +31,5 @@
         <div class="signature-line"></div>
     </div>
 </div>
+@endif
 @endif
