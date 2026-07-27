bfe10a0311131dbd79f10ca06093878b73b777fc
brijeshcode <brijeshchaturvedi.it@gmail.com>
2026-07-27 10:14:21 +0530

    comment note add for tax and non tax field

--- app/Models/Customers/SaleItems.php
+++ app/Models/Customers/SaleItems.php
@@ -47,9 +47,9 @@ class SaleItems extends Model
         'tax_amount_usd',
         'total_tax_amount',
         'total_tax_amount_usd',
-        'total_net_sell_price',
+        'total_net_sell_price',// without tax
         'total_net_sell_price_usd',
-        'total_price',
+        'total_price', // with tax
         'total_price_usd',
         'unit_profit',
         'total_profit',
